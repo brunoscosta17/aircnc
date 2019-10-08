98db5137c83a0b42fc342baa08beed55a5a320cc
= <=>
2019-10-07 21:45:49 -0300

    Finalizado frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,16 @@ import './App.css';
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
   return (
     <div className="container">
       <img src={logo} alt="Aircnc" />
 
       <div className="content">
-        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-        <form>
-          <label htmlFor="email">E-Mail *</label>
-          <input type="email" id="email" placeholder="Seu e-mail" />
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
+
       </div>
     </div>
   );
